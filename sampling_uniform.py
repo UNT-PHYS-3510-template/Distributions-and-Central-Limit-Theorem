824f1863c721843df1f87b019272355cbe9475a6
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2020-11-23 11:22:22 -0600

    Update and rename binomial_distribution.py to sampling_uniform.py

---
+++ sampling_uniform.py
@@ -0,0 +1,35 @@
+# Week 14 Assigment A
+# Sampling from the uniform distribution
+import numpy as np
+import matplotlib.pyplot as plt
+#
+def uniform(x):
+  """
+  This function evaluates the uniform distribution function for the given
+  value of x
+  """
+  return u
+#
+# Generate a plot for the uniform distribution function (u(x) vs. x)
+#
+x = 
+#
+u = 
+#
+# Create one sample of n=10 points drawn from the uniform distribution using the numpy.random.random() function
+#
+n = 10
+#
+sample = 
+#
+# Generate m=100 samples of n=10 points. For each sample compute the mean and collect all the means in a list (or array). 
+#
+means = []
+#
+# Given the list/array of means, compute its frequency distribution
+#
+bins = 
+frequencies = 
+#
+# Compare the frequency distribution of the means of the samples with the result of the central limit theorem
+#
